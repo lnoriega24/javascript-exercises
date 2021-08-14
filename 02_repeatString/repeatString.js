ce4a167adfd20b00676ec6a6a3ebd0e6f6e5ba62
Luis Noriega <luno24@live.com>
2021-08-14 18:23:09 -0300

    Finished repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let i = 0, string2 = '', error = 'ERROR';
+    if(!(num<0)){
+        for (i = 0; i < num; i ++){
+            string2 = string2 + string;
+        }
+        return string2;
+    }
+    return error;
 };
+
+repeatString('hey',3);
 
 module.exports = repeatString;
