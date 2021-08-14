86efd2053d526372a353f47b396f30d51e876d4b
Luis Noriega <luno24@live.com>
2021-08-14 18:58:53 -0300

    Finished reverString tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
-
+    let string = '', j = str.length;
+    for (i = j -1 ; i >= 0 ; i--){
+        string += str[i];
+    }
+    return string;
 };
+
+reverseString('hello');
 
 module.exports = reverseString;
