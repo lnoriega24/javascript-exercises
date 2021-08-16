d01792a68daa7231c6db56cb6610984a1b171db1
Luis Noriega <luno24@live.com>
2021-08-16 18:57:07 -0300

    Finished tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    temp = (temp - 32)*(5 / 9);
+  return parseFloat(temp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    temp = ((temp* 9 / 5) + 32);
+  return parseFloat(temp.toFixed(1));
 };
+
+ftoc(0);
+ctof(0);
 
 module.exports = {
   ftoc,
