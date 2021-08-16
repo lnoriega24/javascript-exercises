0c56bda4d239fb80620b9c0e26c0ebf55d95693d
Luis Noriega <luno24@live.com>
2021-08-16 18:57:43 -0300

    Finished leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 };
+
+console.log(leapYears(1));
 
 module.exports = leapYears;
